9547920c182e2efda149c54314e77a7f4d5f8f76
Caitlin Harper <caitlin.harper@uqconnect.edu.au>
2021-06-13 15:59:11 +1000

    Day 4, part 2

--- src/main/java/charper/advent19/day4/DayFour.java
+++ src/main/java/charper/advent19/day4/DayFour.java
@@ -9,12 +9,23 @@ public class DayFour {
 
     public DayFour() {
         System.out.println(partOne());
+        System.out.println(partTwo());
     }
 
     private Integer partOne() {
         List<Integer> matches = new ArrayList<>();
         for (int i = START; i <= END; i++) {
             if (isDigitsIncreasing(i) && isTwoAdjacentDigitsSame(i)) {
+                matches.add(i);
+            }
+        }
+        return matches.size();
+    }
+
+    private Integer partTwo() {
+        List<Integer> matches = new ArrayList<>();
+        for (int i = START; i <= END; i++) {
+            if (isDigitsIncreasing(i) && exactlyTwoAdjacentDigitsSame(i)) {
                 matches.add(i);
             }
         }
@@ -39,6 +50,24 @@ public class DayFour {
             password / 100 % 10 == password/ 1000 % 10 ||
             password / 1000 % 10 == password / 10000 % 10 ||
             password / 10000 % 10 == password / 100000) 
+        {
+            return true;
+        }
+        return false;   
+    }
+
+    private boolean exactlyTwoAdjacentDigitsSame(int password) {
+        int ones = password % 10;
+        int tens = password / 10 % 10;
+        int huns = password / 100 % 10;
+        int thous = password / 1000 % 10;
+        int ten_thous = password / 10000 % 10;
+        int hun_thous = password / 100000;
+        if ((ones == tens && tens != huns) ||
+            (ones != tens && tens == huns && huns != thous) ||
+            (tens != huns && huns == thous && thous != ten_thous) ||
+            (huns != thous && thous == ten_thous && ten_thous != hun_thous) ||
+            (thous != ten_thous && ten_thous == hun_thous)) 
         {
             return true;
         }
